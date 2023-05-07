af3c83f8b692b5cecc43919c00b205dbbdac2a03
frane <s24185@pjwstk.edu.pl>
2023-05-07 19:14:42 +0200

    fixed some shit it works

--- WorkingV/old_Board.py
+++ WorkingV/old_Board.py
@@ -7,10 +7,25 @@ def generate_starting_set(table=None,):
     for row in range(len(table)):
         for column in range(len(table)):
             randomshit = random.randrange(0,10)
-            if randomshit < 7:
+            if randomshit < 12:
                 value = 0
             else:
                 value = 1
             table[row][column] = value
            
+    return table
+
+def generate_random_board(table = None):
+    if table is None:
+        table = [[]]
+    # up and down
+    for row in range(len(table)):
+        for column in range(len(table)):
+            randomshit = random.randrange(0, 10)
+            if randomshit < 6:
+                value = 0
+            else:
+                value = 1
+            table[row][column] = value
+
     return table
--- WorkingV/old_main.py
+++ WorkingV/old_main.py
@@ -19,18 +19,22 @@ def color_is():
     b = random.randrange(0, 255)
     return (r, g, b)
 
+info = pygame.display.Info()
+height = info.current_h
 
+# Print the height
+print(height)
 # Set the dimensions of each square
-SQUARE_SIZE = 10
+SQUARE_SIZE = 15
 # Set the number of squares in each row and column
-n = 75
+n = 48
 
 WINDOW_SIZE = (n * SQUARE_SIZE, n * SQUARE_SIZE)
-
+pygame.display.set_caption("Conway Game Of Life")
 # Table of Tables
 table = [[0 for j in range(n)] for i in range(n)]
 temporary_table = [[0 for j in range(n)] for i in range(n)]
-clicked_squares = []
+save_table =[[0 for j in range(n)] for i in range(n)]
 
 def draw_square(screen, row, col, color):
     x = col * SQUARE_SIZE
@@ -47,6 +51,11 @@ def draw_screen(screen, table):
             else:
                 color = WHITE
             draw_square(screen, row, column, color)
+            # draw lines between squares
+            pygame.draw.line(screen, (200, 200, 200), (column * SQUARE_SIZE, row * SQUARE_SIZE),
+                             ((column + 1) * SQUARE_SIZE, row * SQUARE_SIZE), 1)
+            pygame.draw.line(screen, (200, 200, 200), (column * SQUARE_SIZE, row * SQUARE_SIZE),
+                             (column * SQUARE_SIZE, (row + 1) * SQUARE_SIZE), 1)
 
 
 def calculate_new_state(table, temp_table):
@@ -62,6 +71,9 @@ def copyTable(tableToCopy, tablefromCopy):
             tableToCopy[row][column] = tablefromCopy[row][column]
 
 
+
+copyTable(save_table,table)
+
 # Create the pygame window
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
@@ -69,9 +81,21 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 screen.fill(WHITE)
 
 font = pygame.font.Font(None, 36)
-restart_button = pygame.Rect(10, 10, 100, 50)
-restart_text = font.render("PAUSE", True, BLACK)
+pause_button = pygame.Rect(10, 10, 100, 50)
+pause_text = font.render("START", True, BLACK)
 
+restart_button = pygame.Rect(120, 10, 120, 50)
+restart_button_text = font.render("RESTART", True, BLACK)
+
+random_button = pygame.Rect(260, 10, 120, 50)
+random_text = font.render("RANDOM", True, BLACK)
+
+
+save_button = pygame.Rect(400, 10, 120, 50)
+save_text = font.render("SAVE", True, BLACK)
+
+reload_button = pygame.Rect(540, 10, 120, 50)
+reload_text = font.render("RELOAD", True, BLACK)
 # Run the game loop
 running = True
 
@@ -80,11 +104,10 @@ running = True
 
 
 def start_game():
-    global clicked_squares
     generate_starting_set(table)
     copyTable(temporary_table, table)
     draw_screen(screen, table)
-    clicked_squares = []
+
 
 def check_if_dead():
     for row in range(len(table)):
@@ -93,21 +116,34 @@ def check_if_dead():
                 return True
     return False
 
-
+pause = True
 start_game()
-pause = False;
 while running:
     if not pause:
         calculate_new_state(table, temporary_table)
         draw_screen(screen, temporary_table)
-
         copyTable(table, temporary_table)
-        pygame.draw.rect(screen, WHITE, restart_button)
 
-    screen.blit(restart_text, (restart_button.x + 10, restart_button.y + 10))
+    pygame.draw.rect(screen, WHITE, pause_button)
+    screen.blit(pause_text, (pause_button.x + 10, pause_button.y + 10))
+
+    pygame.draw.rect(screen, WHITE, restart_button)
+    screen.blit(restart_button_text, (restart_button.x + 10, restart_button.y + 10))
+
+    pygame.draw.rect(screen, WHITE, random_button)
+    screen.blit(random_text, (random_button.x + 10, random_button.y + 10))
+
+    pygame.draw.rect(screen, WHITE, save_button)
+    screen.blit(save_text, (save_button.x + 10, save_button.y + 10))
+
+    pygame.draw.rect(screen, WHITE, reload_button)
+    screen.blit(reload_text, (reload_button.x + 10, restart_button.y + 10))
 
     pygame.display.flip()
-    time.sleep(0.3)
+    if  pause:
+        time.sleep(0)
+    else:
+        time.sleep(0.1)
     if not check_if_dead():
         time.sleep(1)
         start_game()
@@ -115,11 +151,48 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            row = int(event.pos[1]/SQUARE_SIZE)
+            column = int(event.pos[0]/SQUARE_SIZE)
+            if table[row][column] == 1:
+                table[row][column] = 0
+                temporary_table[row][column] = 0
+                draw_square(screen, row, column, (255, 0, 0))
+
+            else:
+                table[row][column] = 1
+                temporary_table[row][column] = 1
+                draw_square(screen, row, column, (0, 255, 0))
+
+            pygame.display.flip()
+
+
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            if restart_button.collidepoint(pos):
+            if pause_button.collidepoint(pos):
+                print("yes")
                 pause = not pause
-                print("mommy")
+                if pause:
+                    pause_text = font.render("START", True, BLACK)
+                else:
+                    pause_text = font.render("PAUSE", True, BLACK)
+            if restart_button.collidepoint(pos):
+                start_game()
+                pause = True
+                pause_text = font.render("START", True, BLACK)
+            if random_button.collidepoint(pos):
+                start_game()
+                table = generate_random_board(table)
+                pause = False
+                pause_text = font.render("PAUSE", True, BLACK)
+            if save_button.collidepoint(pos):
+                copyTable(save_table,table)
+            if reload_button.collidepoint(pos):
+                print("hi")
+                table = save_table
+                temporary_table = save_table
+                draw_screen(screen,table)
+                pygame.display.flip()
 
 # Quit pygame
 pygame.quit()
